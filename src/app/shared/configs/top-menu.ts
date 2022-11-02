241e77e4000fb1f3e997fed18a7046d6bb2c3449
CocoOreo <cocooreo1028@gmail.com>
2022-11-01 22:11:58 -0400

    feat: Adding the top-menu

---
+++ src/app/shared/configs/top-menu.ts
@@ -0,0 +1,29 @@
+import { MenuItem } from '../components/top-menu/top-menu.component';
+
+export const topMenu: MenuItem[] = [
+  {
+    title: 'Popular',
+    img: 'assets/images/popular.svg',
+    active: 'assets/images/popular-active.svg',
+  },
+  {
+    title: 'Chair',
+    img: 'assets/images/chair.svg',
+    active: 'assets/images/chair-active.svg',
+  },
+  {
+    title: 'Table',
+    img: 'assets/images/table.svg',
+    active: 'assets/images/table-active.svg',
+  },
+  {
+    title: 'Armchair',
+    img: 'assets/images/armchair.svg',
+    active: 'assets/images/armchair-active.svg',
+  },
+  {
+    title: 'Bed',
+    img: 'assets/images/bed.svg',
+    active: 'assets/images/bed-active.svg',
+  },
+];
