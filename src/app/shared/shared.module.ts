28f327ffb2b0fd5e41c9c84aac9fc4c75cee45ee
CocoOreo <cocooreo1028@gmail.com>
2022-11-02 15:00:15 -0400

    feat: Adding the footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [TopMenuComponent],
+  declarations: [TopMenuComponent, FooterComponent],
   imports: [CommonModule, FormsModule],
-  exports: [CommonModule, FormsModule, TopMenuComponent],
+  exports: [CommonModule, FormsModule, TopMenuComponent, FooterComponent],
 })
 export class SharedModule {}
