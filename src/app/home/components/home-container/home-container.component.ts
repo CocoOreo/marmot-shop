3a05228a376278886d2e3517a432feb6bd854fb5
CocoOreo <cocooreo1028@gmail.com>
2022-11-07 19:23:25 -0500

    feat: Adding the home page

---
+++ src/app/home/components/home-container/home-container.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { topMenu } from 'src/app/shared/configs/top-menu';
+import { MenuItem } from 'src/app/shared/components/top-menu/top-menu.component';
+
+@Component({
+  selector: 'app-home-container',
+  templateUrl: './home-container.component.html',
+  styleUrls: ['./home-container.component.scss'],
+})
+export class HomeContainerComponent {
+  menu = topMenu;
+
+  list = [
+    {
+      img: 'assets/images/lamp.svg',
+      price: 1356.89,
+      title: 'Minimal Stand',
+      id: 2,
+    },
+    {
+      img: 'assets/images/lamp.svg',
+      price: 1356.89,
+      title: 'Minimal Stand',
+      id: 3,
+    },
+    {
+      img: 'assets/images/lamp.svg',
+      price: 1356.89,
+      title: 'Minimal Stand',
+      id: 4,
+    },
+    {
+      img: 'assets/images/lamp.svg',
+      price: 1356.89,
+      title: 'Minimal Stand',
+      id: 5,
+    },
+    {
+      img: 'assets/images/lamp.svg',
+      price: 1356.89,
+      title: 'Minimal Stand',
+      id: 6,
+    },
+    {
+      img: 'assets/images/lamp.svg',
+      price: 1356.89,
+      title: 'Minimal Stand',
+      id: 7,
+    },
+  ];
+
+  handleTabSelected(item: MenuItem) {
+    console.log('Select', item);
+  }
+  constructor() {}
+}
